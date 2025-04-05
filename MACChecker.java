79af698df234e9c7772306c60da74b9250a05d69
PedroTFX <pedro.trindade.tfx@gmail.com>
2025-04-05 14:46:15 +0100

    all macs functionalities DONE - NOT TESTED

--- MACChecker.java
+++ MACChecker.java
@@ -1,5 +1,8 @@
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Base64;
 
@@ -15,17 +18,21 @@ public class MACChecker {
     // this function will check the folder MAC_Values
     // compare the values with the values of the files users.txt
     // and all workspaces
-    public static boolean checkMAC(String filename, String password) throws Exception{
-        String f = Paths.get(filename).getFileName().toString();
-        String newMAC = generateMAC(f, generateAESKeyFromPassword(password, password.getBytes()).toString());
+    public static boolean checkMAC(String pathToFile, String password) throws Exception{
+        String fileName = Paths.get(pathToFile).getFileName().toString();
+        System.out.println("fileName: " + fileName);
+        String newMAC = generateMAC(pathToFile, generateAESKeyFromPassword(password, password.getBytes()).toString());
 
         // read the MAC file
-        File directory = new File("macs/");
+        // System.out.println("pathToFile: " + pathToFile);
+        // System.out.println("fileName: " + fileName);
+        // System.out.println("macs/" + pathToFile.substring(0, pathToFile.length() - fileName.length() - 1));
+        File directory = new File("macs/" + pathToFile.substring(0, pathToFile.length() - fileName.length() - 1));
         File[] files_in_dir = directory.listFiles();
         File matchingFile = null;
         for(File file : files_in_dir) {
             // System.out.println("file: " + file.getName());
-            if (!file.isDirectory() && file.getName().split("\\.")[0].equals(f.split("\\.")[0])) {
+            if (!file.isDirectory() && file.getName().split("\\.")[0].equals(fileName.split("\\.")[0])) {
                 matchingFile = file;
                 break; // Exit the loop once we find a matching file
             }
@@ -33,7 +40,7 @@ public class MACChecker {
 
         // check if the file is null
         if (matchingFile == null) {
-            System.out.println("No matching MAC files found with:" + f);
+            System.out.println("No matching MAC files found with:" + fileName);
             return false;
         }
 
@@ -52,12 +59,12 @@ public class MACChecker {
     public static String generateMAC(String filePath, String secretKey) throws Exception {
         byte[] fileContent = Files.readAllBytes(Paths.get(filePath));
 
-        System.out.println("filePath: " + filePath);
-        System.out.println(Paths.get(filePath).getFileName().toString());
+        // System.out.println("filePath: " + filePath);
+        // System.out.println(Paths.get(filePath).getFileName().toString());
 
         // test with print
-        System.out.println("filePath: " + filePath);
-        System.out.println("fileContent: " + new String(fileContent));
+        // System.out.println("filePath: " + filePath);
+        // System.out.println("fileContent: " + new String(fileContent));
 
         Mac mac = Mac.getInstance("HmacSHA256");
         SecretKeySpec keySpec = new SecretKeySpec(secretKey.getBytes(), "HmacSHA256");
@@ -68,32 +75,69 @@ public class MACChecker {
     }
 
     public static boolean updateMAC(String filePath, String password) throws Exception {
-        // Generate new MAC for the file
-        // System.out.println("filepath: " + filePath);
-        String newMAC = generateMAC(filePath, generateAESKeyFromPassword(password, password.getBytes()).toString());
-        // System.out.println("newMAC: " + newMAC);
-        try {
-            String file = filePath.split("/")[filePath.split("/").length - 1];
-            String workspace = filePath.substring(0, filePath.length() - file.length() - 1);
-            System.out.println("workspace: " + workspace);
-            System.out.println("file: " + file);
-
-            File macFile = new File("macs/" + workspace + "/" + file.split("\\.")[0] + ".mac");
-            Files.write(macFile.toPath(), newMAC.getBytes());
-        }catch (Exception e) {
-            System.out.println("Error updating MAC: " + e.getMessage());
-            return false;
+        // check if the file exists
+        if (!new File(filePath).exists()) {
+            System.out.println("File does not exist: <" + filePath + ">, .mac to be deleted.");
+            return removeMAC(filePath, password);
         }
+
+        // Generate new MAC for the file
+        String newMAC = generateMAC(filePath, generateAESKeyFromPassword(password, password.getBytes()).toString());
+    
+        // Extract file and workspace information
+        String file = filePath.split("/")[filePath.split("/").length - 1];
+        String workspace = filePath.substring(0, filePath.length() - file.length() - 1);
+        // System.out.println("workspace: " + workspace);
+        // System.out.println("file: " + file);
+    
+        // Create the MAC file
+        File macFile = new File("macs/" + workspace + "/" + file.split("\\.")[0] + ".mac");
+    
+        // Ensure the parent directories exist
+        File parentDir = macFile.getParentFile();
+        if (parentDir != null && !parentDir.exists()) {
+            if (parentDir.mkdirs()) {
+                System.out.println("Directories created: " + parentDir.getAbsolutePath());
+            } else {
+                System.out.println("Failed to create directories: " + parentDir.getAbsolutePath());
+                return false;
+            }
+        }
+    
+        // Create the MAC file if it doesn't exist
+        if (!macFile.exists()) {
+            if (macFile.createNewFile()) {
+                System.out.println("MAC file created: " + macFile.getAbsolutePath());
+            } else {
+                System.out.println("Failed to create MAC file: " + macFile.getAbsolutePath());
+                return false;
+            }
+        }
+    
+        // Write the new MAC to the file
+        Files.write(macFile.toPath(), newMAC.getBytes());
         return true;
     }
 
-    public static File getMACFile(String filename) {
-        // Get the MAC file for the given filename
+    private static boolean removeMAC(String filePath, String password) {
+        // Extract file and workspace information
+        String file = filePath.split("/")[filePath.split("/").length - 1];
+        String workspace = filePath.substring(0, filePath.length() - file.length() - 1);
+        // System.out.println("workspace: " + workspace);
+        // System.out.println("file: " + file);
+
+        // Create the MAC file
+        File macFile = new File("macs/" + workspace + "/" + file.split("\\.")[0] + ".mac");
+        return macFile.exists() && macFile.delete();
+    }
+
+    public static File getMACFile(String pathToFile) {
+        // Get the MAC file for the given pathToFile
         File directory = new File("macs/");
         File[] files_in_dir = directory.listFiles();
         File matchingFile = null;
         for(File file : files_in_dir) {
-            if (!file.isDirectory() && file.getName().split("\\.")[0].equals(filename.split("\\.")[0])) {
+            if (!file.isDirectory() && file.getName().split("\\.")[0].equals(pathToFile.split("\\.")[0])) {
                 matchingFile = file;
                 break; // Exit the loop once we find a matching file
             }
@@ -165,8 +209,12 @@ public class MACChecker {
         // System.out.println(allCheckMACs("users.txt"));              //false
         // System.out.println(updateMAC("workspaces/UGA:admin>admin,user1/uga.txt", "users.txt"));     //true
         // System.out.println(allCheckMACs("users.txt"));              //true
-
+        
+        // System.out.println("checkMac: " + checkMAC("workspaces/UGA:admin>admin,user1/uga.txt", "users.txt")); // true
+        System.out.println("updateMac: " + updateMAC("workspaces/UGA:admin>admin,user1/uga.txt", "users.txt")); // true
+        // System.out.println("checkMac: " + checkMAC("workspaces/UGA:admin>admin,user1/uga.txt", "users.txt")); // true
+        
         // System.out.println(generateMAC("workspaces/" + Workspaces.findWorkspace("UGA") + "/uga.txt", "users.txt"));
-        System.out.println(updateMAC("workspaces/" + Workspaces.findWorkspace("UGA") + "/uga.txt", "users.txt"));
+        // System.out.println(updateMAC("workspaces/" + Workspaces.findWorkspace("UGA") + "/uga.txt", "users.txt"));
     }   
 }
